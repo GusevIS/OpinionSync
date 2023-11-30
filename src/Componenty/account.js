f35c3df7f14ac124fe0f49bda3227fb737e11b51
Jakub Jusko <jakub.jusko@centrum.cz>
2023-11-30 01:13:57 +0100

    Account - complete site

--- src/Componenty/account.js
+++ src/Componenty/account.js
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom";
 import Button from "../UI/button";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Account = () => {
     return (
        <>
-        <div style={styles.box_email}>
+           <div style={styles.greeting}>
+               <p>Welcome User!</p>
+           </div>
+
+            <div style={styles.icon}>
+                <img src="https://icon-library.com/images/blank-person-icon/blank-person-icon-7.jpg" alt="person"/>
+            </div>
+
+           <div style={styles.box_email}>
             <p style={styles.text}>*User email*</p>
         </div>
 
     <div style={styles.box_username}>
         <p style={styles.text}>*Username*</p>
     </div>
-           <div className="mt-80 flex flex-row items-center gap-2">
+           <div className="mt-52 flex flex-row items-center gap-2">
            <Button to="" className="w-40">
                Change email
            </Button>
@@ -31,7 +40,7 @@ const Account = () => {
             backgroundColor: '#cbd5e1',
             width: '322px',
             position: 'relative',
-            top: '300px'
+            top: '190px'
         },
         box_username:
             {
@@ -41,7 +50,7 @@ const Account = () => {
                 backgroundColor: '#cbd5e1',
                 width: '322px',
                 position: 'relative',
-                top: '310px'
+                top: '200px'
 
             },
         text: {
@@ -49,6 +58,20 @@ const Account = () => {
             fontSize: '16px',
             margin: '0',
             textAlign: 'center'
+        },
+        greeting: {
+            color: '#000000',
+            fontSize: '45px',
+            position: 'relative',
+            right: '200px',
+            top: '80px'
+        },
+        icon:{
+            height: '300px',
+            width: '220px',
+            position: 'relative',
+            left: '43px',
+            top: '250px'
         }
     }
 export default Account;
