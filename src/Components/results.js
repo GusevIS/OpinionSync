4c0c862055e1e63a8a7fc66a905eaf5a3b73051f
Jakub Jusko <jakub.jusko@centrum.cz>
2023-12-07 11:34:54 +0100

    Pathway to enter site "discussion" added

--- src/Components/results.js
+++ src/Components/results.js
@@ -2,6 +2,7 @@ import React from "react";
 import ResultsTable from "./tables/resultsTable";
 import ResultsUserList from "./lists/resultsUserList";
 import ShareLink from "../UI/shareLink";
+import ContainedButton from "../Components/buttons/containedButton";
 
 
 
@@ -17,10 +18,16 @@ const Results = () => {
             </ResultsTable>
             <div><p style={{ color: '#0A2440', fontFamily: 'Sen, sans-serif', fontWeight: 'bold', fontSize: '18px'}}>Vyberte uživatele pro porovnání odpovědí</p>
                 <ResultsUserList>
-
                 </ResultsUserList>
-                <ShareLink />
+
+                    <div className = "mt-7">
+                <ContainedButton buttonText="Enter discussion" to="/discussion" customWidth="250px" customHeight="55px">
+                    {}
+                </ContainedButton>
+                    </div>
             </div>
+                <ShareLink />
+
 
 
         </div>
